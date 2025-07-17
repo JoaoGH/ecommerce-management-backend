99d49793086cd7a88c91215269848db745e59704
JoaoGH <gabrielhartmannqwert@gmail.com>
2025-07-17 12:19:22 -0300

    feat: adicionar validação de valor e quantidade de itens

--- src/main/java/br/com/foursales/ecommerce/service/ProdutoService.java
+++ src/main/java/br/com/foursales/ecommerce/service/ProdutoService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Service
@@ -17,5 +18,24 @@ public class ProdutoService extends DefaultCrudService<Produto, UUID> {
 	@Override
 	protected JpaRepository<Produto, UUID> getRepository() {
 		return repository;
+	}
+
+	@Override
+	protected void beforeSave(Produto entity) {
+		beforeSaveAndUpdate(entity);
+	}
+
+	@Override
+	protected void beforeUpdate(Produto entity) {
+		beforeSaveAndUpdate(entity);
+	}
+
+	protected void beforeSaveAndUpdate(Produto entity) {
+		if (entity.getQuantidadeEmEstoque() < 0) {
+			throw new RuntimeException("Valor inválido para quantidade de itens em estoque do produto.");
+		}
+		if (entity.getPreco().compareTo(BigDecimal.ZERO) < 0) {
+			throw new RuntimeException("Valor inválido para preço do produto.");
+		}
 	}
 }
