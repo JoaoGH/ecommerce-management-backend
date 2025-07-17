f6f29ef3606aa6f9beac607792de254ef4f739e0
JoaoGH <gabrielhartmannqwert@gmail.com>
2025-07-17 19:54:14 -0300

    fix: adicionar anotacao de mapping

--- src/main/java/br/com/foursales/ecommerce/controller/PedidoController.java
+++ src/main/java/br/com/foursales/ecommerce/controller/PedidoController.java
@@ -5,10 +5,12 @@ import br.com.foursales.ecommerce.service.DefaultCrudService;
 import br.com.foursales.ecommerce.service.PedidoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.UUID;
 
 @Controller
+@RequestMapping("/pedido")
 @RequiredArgsConstructor
 public class PedidoController extends DefaultCrudController<Pedido, UUID> {
 
